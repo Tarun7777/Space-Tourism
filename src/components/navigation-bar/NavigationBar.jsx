61d75ecaba9d59da3af7a216fbdbf550cec13dd2
Sai Tarun Tavva <taruntavva333@gmail.com>
2022-03-24 20:21:33 +0530

    fix(ui): app and navigation bar styles

--- src/components/navigation-bar/NavigationBar.jsx
+++ src/components/navigation-bar/NavigationBar.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
+import Logo from '../../assets/shared/logo.ico';
 import { content } from '../../constants/content';
 import './NavigationBar.css';
 
 const NavigationBar = ({ isMain = false }) => {
   return (
     <div className='header flex'>
-      {isMain && (
-        <img className='logo' src='../../assets/shared/logo.ico' alt='Logo' />
-      )}
+      {isMain && <img className='logo' src={Logo} alt='Logo' />}
       <nav className='nav-bar'>
         <ul className='nav-items flex'>
           {content?.main?.navItems.map((item, index) => (
