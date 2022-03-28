c3125d6b03a15491afdab02a48545faef70720a2
Sai Tarun Tavva <taruntavva333@gmail.com>
2022-03-28 23:01:05 +0530

    style: home scss changes

--- src/components/home/molecules/left-panel/LeftPanel.jsx
+++ src/components/home/molecules/left-panel/LeftPanel.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { content } from '../../../../constants';
-import './LeftPanel.css';
+import './LeftPanel.scoped.scss';
 
 const WelcomeText = () => {
   return (
--- src/components/home/molecules/right-panel/RightPanel.jsx
+++ src/components/home/molecules/right-panel/RightPanel.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { content, DESTINATION } from '../../../../constants';
-import './RightPanel.css';
+import './RightPanel.scoped.scss';
 
 const Button = () => {
   const navigate = useNavigate();
